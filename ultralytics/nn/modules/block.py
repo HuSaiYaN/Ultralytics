3c1139dffc751ce7e663b20746f2644b3c7e4397
HuSaiYaN <161064587+HuSaiYaN@users.noreply.github.com>
2024-02-23 23:36:12 +0530

    Update block.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -131,22 +131,6 @@ class HGBlock(nn.Module):
         return y + x if self.add else y
 
 
-# class SPP(nn.Module):
-#     """Spatial Pyramid Pooling (SPP) layer https://arxiv.org/abs/1406.4729."""
-
-#     def __init__(self, c1, c2, k=(5, 9, 13)):
-#         """Initialize the SPP layer with input/output channels and pooling kernel sizes."""
-#         super().__init__()
-#         c_ = c1 // 2  # hidden channels
-#         self.cv1 = Conv(c1, c_, 1, 1)
-#         self.cv2 = Conv(c_ * (len(k) + 1), c2, 1, 1)
-#         self.m = nn.ModuleList([nn.MaxPool2d(kernel_size=x, stride=1, padding=x // 2) for x in k])
-
-#     def forward(self, x):
-#         """Forward pass of the SPP layer, performing spatial pyramid pooling."""
-#         x = self.cv1(x)
-#         return self.cv2(torch.cat([x] + [m(x) for m in self.m], 1))
-
 class SPP(nn.Module):
     """Spatial Pyramid Pooling (SPP) layer https://arxiv.org/abs/1406.4729."""
 
